55a65b811ff1426e918986c8fabdc7dfa141acdb
Daniel_Guerrero <dag83@njit.edu>
2025-06-29 17:03:40 -0400

    Started HTML page and JS page, to be updated

---
+++ script.js
@@ -0,0 +1,4 @@
+//Important HTML elements
+const addButtonOne = document.getElementById("vOneOut");
+
+//document.getElementById("vOneIn").insertAdjacentElement="<p>Marx talks about it in das kapital</p>";
