aab622e3d0151e4560c3507bc71fa585d220513a
Daniel_Guerrero <dag83@njit.edu>
2025-06-29 18:12:59 -0400

    Added functional buttons that update the list, which is now ordered

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 //Important HTML elements
-const addButtonOne = document.getElementById("vOneOut");
+const inOne = document.getElementById("vOneIn");
+const inTwo = document.getElementById("vTwoIn");
 
-//document.getElementById("vOneIn").insertAdjacentElement="<p>Marx talks about it in das kapital</p>";
+const addButtonOne = document.getElementById("vOneOut");
+const addButtonTwo = document.getElementById("vTwoOut");
+
+const listOne = document.getElementById("vOne");
+const listTwo = document.getElementById("vTwo");
+
+//Button click event listeners
+/*const collecto = (list, base) =>{
+    const newVal=base.value;
+        const newItem=document.createElement('li');
+        newItem.textContent = newVal;
+        list.appendChild(newItem);
+        base.value=0;
+}
+addButtonOne.addEventListener("click", collecto(listOne,inOne));*/
+function collecto(input, list){
+    console.log("Is this even functional?");
+    const newVal=input.value;
+    const newItem=document.createElement('li');
+    newItem.textContent = newVal;
+    list.appendChild(newItem);
+    input.value=0;
+}
+
+addButtonOne.addEventListener("click", () => {collecto(inOne, listOne)});
+addButtonTwo.addEventListener("click", () => {collecto(inTwo, listTwo)});
