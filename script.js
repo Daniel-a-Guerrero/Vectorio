75b830be42de34301e36593aab88171190b2acf5
Daniel_Guerrero <dag83@njit.edu>
2025-07-08 12:59:32 -0400

    Begun process of in earnest the scalar mult part

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+/**
+ * Vectorio: A page for what I learn from the linear algebra
+ * 
+ * A single web page utilizing vanilla HTML and JS to display functionally what I learn from my Linear Algebra textbook
+ * Might add more pages corresponding to more chapters
+ * 
+ * Chapter 1, Vectors in R^n, C^n, spatial vectors:
+ *      Vector Addition (C)
+ *      Vector Multiplication (C)
+ *      
+ */
+
+
 //Important HTML elements
+
+    //Number input regions
 const inOne = document.getElementById("vOneIn");
 const inTwo = document.getElementById("vTwoIn");
+    //Select Vector
+const vecSel = document.getElementById("vChoice");
 
-
+    //Buttons
+    //Vector Input
 const addButtonOne = document.getElementById("vOneOut");
 const addButtonTwo = document.getElementById("vTwoOut");
-
+    //Addition button
 const vectorAdditionButton = document.getElementById("calcAdd");
+    //Scalar Multiplication Button
+const scalarMultButton = document.getElementById("calcScMult");
 
 
 const listOne = document.getElementById("vOne");
@@ -21,8 +41,12 @@ const vS=[];
 
 //Button click event listeners for taking initial values
 
-function collecto(input, list,arr){
-    //console.log("Is this even functional?");
+function collecto(input, list, arr, identity=""){
+    //Determine if lists are empty, and if not, put them into the selector for scalar multiplication
+    if(arr.length!==0)
+    {
+        ;
+    }
     let newVal=input.value;
     if(newVal === ''){
         newVal=0;
@@ -38,8 +62,8 @@ function collecto(input, list,arr){
     console.log(arr)
 }
 
-addButtonOne.addEventListener("click", () => {collecto(inOne, listOne, v1)});
-addButtonTwo.addEventListener("click", () => {collecto(inTwo, listTwo, v2)});
+addButtonOne.addEventListener("click", () => {collecto(inOne, listOne, v1, "v1")});
+addButtonTwo.addEventListener("click", () => {collecto(inTwo, listTwo, v2, "v2")});
 
 //Listener for vector addition
 
@@ -48,7 +72,7 @@ const add = (a1, a2) =>{
     let contento=vS;
     if(a1.length !== a2.length){
         contento="Must add two vectors of equal length";
-        const it=document.createElement('li');
+        const it=document.createElement('p');
         it.textContent=contento;
         vectorAdditionList.appendChild(it);
     }
@@ -69,3 +93,6 @@ const add = (a1, a2) =>{
     }
 };
 vectorAdditionButton.addEventListener("click", () => add(v1,v2));
+
+
+//Scalar multiplication with vectors
